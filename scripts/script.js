efbfffad2655f3a93c8b07f2f5e480b2b9f3eccc
DesignofCp <4in1telecommunications@gmail.com>
2022-07-19 17:38:58 -0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ new Vue({
           name: "Cartel Swag",
           artist: "Kevin Gates",
           cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0UMvVPNuKecFmebGKteOMGbJb2pXkxkYIxVpaYRrEKJL6y-LMuQ&s",
-          source: "/mp3/cartel-swag.mp3",
+          source: "https://github.com/DesignofCp/Kevin-Gates-mini-music-player/blob/master/mp3/aint-too-hard.mp3",
           url: "mp3/cartel-sway.mp3",
           favorited: false
         },
